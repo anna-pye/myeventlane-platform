c3cd44d13ba6e7c11c1f07ece6239c0b8019d12a
Anna Pye <113150470+anna-pye@users.noreply.github.com>
2026-08-16 17:01:33 +1000

    Stack public event content cards

--- web/modules/custom/myeventlane_event/tests/src/Unit/PublicEventBodyTemplateContractTest.php
+++ web/modules/custom/myeventlane_event/tests/src/Unit/PublicEventBodyTemplateContractTest.php
@@ -25,4 +25,29 @@ final class PublicEventBodyTemplateContractTest extends TestCase {
     self::assertStringNotContainsString('{{ content.body }}', $template);
   }
 
+  public function testPublicContentCardsUseOneOrderedColumn(): void {
+    $moduleRoot = dirname(__DIR__, 3);
+    $webRoot = dirname($moduleRoot, 3);
+    $template = (string) file_get_contents(
+      $webRoot . '/themes/custom/myeventlane_theme/templates/node/node--event--full.html.twig',
+    );
+    $styles = (string) file_get_contents(
+      $webRoot . '/themes/custom/myeventlane_theme/src/scss/components/_event-full.scss',
+    );
+
+    self::assertStringContainsString('<div class="mel-event-full__content-grid">', $template);
+    self::assertStringNotContainsString('mel-event-full__content-grid--duo', $template);
+    self::assertStringNotContainsString('.mel-event-full__content-grid--duo', $styles);
+
+    $aboutPosition = strpos($template, "'About the Event'|t");
+    $highlightsPosition = strpos($template, "'Highlights'|t");
+    $expectPosition = strpos($template, "'What to expect'|t");
+
+    self::assertIsInt($aboutPosition);
+    self::assertIsInt($highlightsPosition);
+    self::assertIsInt($expectPosition);
+    self::assertLessThan($highlightsPosition, $aboutPosition);
+    self::assertLessThan($expectPosition, $highlightsPosition);
+  }
+
 }
